0275baabf1042764552c0aabb2eca0b5c7a69ede
RhofirAbdelali <abdo.rhofir@gmail.com>
2025-02-09 20:31:36 +0100

    Exercice 7

--- src/commands.ts
+++ src/commands.ts
@@ -1,13 +1,21 @@
 import { Cmd } from 'redux-loop';
 import { fetchCatsCommit, fetchCatsRollback } from './actions';
 import { FetchCatsRequest } from './types/actions.type';
+import { Picture } from './types/picture.type';
 
 export const cmdFetch = (action: FetchCatsRequest) =>
   Cmd.run(
-    () => {
-      return fetch(action.path, {
-        method: action.method,
-      }).then(checkStatus);
+    async () => {
+      const response = await fetch(action.path, { method: action.method });
+      checkStatus(response);
+      const data = await response.json();
+      if (!data.hits) throw new Error("Invalid API response: missing 'hits'");
+      return data.hits.map((hit: any): Picture => ({
+        previewFormat: hit.previewURL,
+        webFormat: hit.webformatURL,
+        largeFormat: hit.largeImageURL,
+        author: hit.user,
+      }));
     },
     {
       successActionCreator: fetchCatsCommit, // (equals to (payload) => fetchCatsCommit(payload))
